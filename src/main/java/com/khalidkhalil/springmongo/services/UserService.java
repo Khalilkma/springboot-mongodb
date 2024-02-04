3bc279f9e1af839e46ef903280381e512c3a3c0e
Khalilkma <khalilkma757.khalid@gmail.com>
2024-02-04 09:58:56 -0300

    User insertion with Post

--- src/main/java/com/khalidkhalil/springmongo/services/UserService.java
+++ src/main/java/com/khalidkhalil/springmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.khalidkhalil.springmongo.services;
 
 import com.khalidkhalil.springmongo.domain.User;
+import com.khalidkhalil.springmongo.dto.UserDTO;
 import com.khalidkhalil.springmongo.repository.UserRepository;
 import com.khalidkhalil.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
