89da69457cf9736ba078d26341b58ba6661e8ab2
Khalilkma <khalilkma757.khalid@gmail.com>
2024-02-03 22:25:01 -0300

    User by Id

--- src/main/java/com/khalidkhalil/springmongo/services/UserService.java
+++ src/main/java/com/khalidkhalil/springmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.khalidkhalil.springmongo.services;
 
 import com.khalidkhalil.springmongo.domain.User;
 import com.khalidkhalil.springmongo.repository.UserRepository;
+import com.khalidkhalil.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
