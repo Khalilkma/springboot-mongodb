4757261cfc170f390a04ddfd3f6f487d0b238588
Khalilkma <khalilkma757.khalid@gmail.com>
2024-02-07 18:15:44 -0300

    Post by Id

--- src/main/java/com/khalidkhalil/springmongo/resources/UserResource.java
+++ src/main/java/com/khalidkhalil/springmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.khalidkhalil.springmongo.resources;
 
+import com.khalidkhalil.springmongo.domain.Post;
 import com.khalidkhalil.springmongo.domain.User;
 import com.khalidkhalil.springmongo.dto.UserDTO;
 import com.khalidkhalil.springmongo.services.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
---
+++ src/main/java/com/khalidkhalil/springmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.khalidkhalil.springmongo.services;
+
+import com.khalidkhalil.springmongo.domain.Post;
+import com.khalidkhalil.springmongo.domain.User;
+import com.khalidkhalil.springmongo.dto.UserDTO;
+import com.khalidkhalil.springmongo.repository.PostRepository;
+import com.khalidkhalil.springmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
