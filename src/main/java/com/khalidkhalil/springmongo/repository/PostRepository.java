8da8fa0da80e2a46d095ad7e545ae8b038a67ec7
Khalilkma <khalilkma757.khalid@gmail.com>
2024-02-09 00:49:19 -0300

    @Query

--- src/main/java/com/khalidkhalil/springmongo/repository/PostRepository.java
+++ src/main/java/com/khalidkhalil/springmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.khalidkhalil.springmongo.repository;
 import com.khalidkhalil.springmongo.domain.Post;
 import com.khalidkhalil.springmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ \"<field>\": { \"$regex\": ?0, \"$options\": \"<options>\" } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
